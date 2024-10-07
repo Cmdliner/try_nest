18dbf44cf0209c8ec3b02331e0fcbbdae6ff8f31
Adeyemi Abiade <abiadeabdulazeez@gmail.com>
2024-10-07 18:01:39 +0100

    Added custom exception filter

--- src/auth/auth.middleware.ts
+++ src/auth/auth.middleware.ts
@@ -1,28 +1,37 @@
-import { Injectable, NestMiddleware, UnauthorizedException } from "@nestjs/common";
-import { NextFunction, Request, Response } from "express";
-import { UserService } from "src/user/user.service";
+import {
+    ForbiddenException,
+    Injectable,
+    NestMiddleware,
+    UnauthorizedException,
+    UseFilters,
+} from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+import { isValidObjectId } from 'mongoose';
+import { UnauthorizedExceptionFilter } from 'src/common/http-exception.filter';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
-
     constructor(private userService: UserService) { }
-
-    use(req: Request, res: Response, next: NextFunction) {
+    
+    @UseFilters(new UnauthorizedExceptionFilter)
+    async use(req: Request, _res: Response, next: NextFunction) {
         try {
-            const [_, authToken] = req.headers.authorization?.split(" ")  || "";
-        const user = this.userService.findOne(authToken);
-        console.log({ user, authToken });
-        if (user) {
-            req.user = user;
-            next();
-        }
-        throw new UnauthorizedException("Unauthorized");
-        } catch (error) {
-            console.error(error);
-            throw new UnauthorizedException("Unauthorized");
+            const authHeader = req.headers.authorization;
+            if (!authHeader || !authHeader.startsWith('Bearer ')) {
+                throw new UnauthorizedException('Auth header required!');
+            }
+            const [_, userId] = authHeader.split(' ');
+            if (!isValidObjectId(userId))
+                throw new ForbiddenException('Unauthorized!');
 
+            const user = await this.userService.findOne(userId);
+            req['user'] = user;
+            next();
+        } catch (error) {
+            // console.error(error);
+            throw new UnauthorizedException('Unauthorized');
         }
-        
+        return 'Error occured';
     }
 }
-
